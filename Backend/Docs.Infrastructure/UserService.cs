d4e395061a8da56de6c7962170603341da508e7b
Pohrib Valentin <pohribvalentin17@yahoo.com>
2019-12-24 20:23:19 +0200

    Added interfaces folder

---
+++ Backend/Docs.Infrastructure/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Docs.Domain.Interfaces;
+using Docs.Domain.Models;
+
+namespace Docs.Infrastructure
+{
+	public class UserService: IUserService
+	{
+		public Task<List<User>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

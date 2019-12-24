b78db6b541458267353f555dcc4d2c9eba328ebf
Ion Rapeanu-Andreescu <rapeanu_ion@yahoo.com>
2019-12-24 22:17:57 +0200

    Add Managers directory in docs.Infrastructure

---
+++ Backend/Docs.Infrastructure/Managers/UserManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Docs.Domain.Interfaces;
+using Docs.Domain.Interfaces.IManagers;
+using Docs.Domain.Models;
+
+namespace Docs.Infrastructure
+{
+	public class UserManager: IUserManager
+	{
+		public Task<List<User>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+        Task<User> IUserManager.AlterAsync(User User)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<User> IUserManager.CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IUserManager.DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<User>> IUserManager.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<User> IUserManager.GetAsync(Expression<Func<User, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

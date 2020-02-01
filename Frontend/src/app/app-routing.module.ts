682e1afd70dfc09951fa81c4e6b014cb96299a66
LordTofi <tofaniulian1@gmail.com>
2020-02-01 16:45:51 +0200

    Started working on FrontEnd

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { UsersComponent } from './users/users.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'courses', component: CoursesComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+  // { path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(public dialog: MatDialog,
+    private router: Router) {}
+
+  ngOnInit() {
+  }
+
+  openDialog(dialogName: string) {
+    const dialogRef = this.dialog.open(dialogName === "login" ? LogInDialog : SignUpDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  goTo(address) {
+    this.router.navigate(['/' + address]);
+  }
+
+}
+
+@Component({
+  selector: 'log-in-dialog',
+  templateUrl: 'log-in-dialog.html',
+})
+export class LogInDialog {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+            '';
+  }
+}
+
+@Component({
+  selector: 'sign-up-dialog',
+  templateUrl: 'sign-up-dialog.html',
+})
+export class SignUpDialog {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  passwordCheck = new FormControl('', [Validators.required]);
+  form = new FormGroup({
+    email: this.email,
+    password: this.password,
+    passwordCheck: this.passwordCheck
+   })
+
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+            '';
+  }
+
+  getPasswordCheckErrorMessage() {
+    return this.passwordCheck.hasError('required') ? 'You must enter a value' :
+        this.passwordCheck.hasError('notMatched') ? 'Passwords do not match' :
+            '';
+  }
+
+  equalPasswords(): boolean {
+
+    const matched: boolean = this.password.value === this.passwordCheck.value;
+
+    console.log('equaltest', matched);
+
+    if (matched) {
+        this.form.controls.passwordCheck.setErrors(null);
+    } else {
+        this.form.controls.passwordCheck.setErrors({
+           notMatched: true
+        });
+    }
+
+    return matched;
+  }
+}

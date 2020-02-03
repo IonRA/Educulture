e01fe26b1d5e4d27bc373d5d292150fedef919a9
Pohrib Valentin <pohribvalentin17@yahoo.com>
2020-02-03 21:54:32 +0200

    added login method to users controller

--- Backend/DocsAPI/Controllers/UsersController.cs
+++ Backend/DocsAPI/Controllers/UsersController.cs
@@ -61,18 +61,37 @@ namespace DocsAPI.Controllers
 		    }
 	    }
 
+	    [HttpPost("Login")]
+	    public async Task<IActionResult> Login([FromBody]User user)
+	    {
+		    
+		    try
+		    {
+			    var _user = await _userManager.GetAsync(x => x.Username == user.Username && x.Password == user.Password);
+
+			    if (_user == null)
+				    return NotFound();
+
+			    return Ok(_user);
+		    }
+		    catch (Exception ex)
+		    {
+			    return StatusCode(500, ex.Message);  
+		    }
+	    }
+
 
         [HttpPost("CreateUser")]
-	    public async Task<IActionResult> CreateUser(User user)
+	    public async Task<IActionResult> CreateUser([FromBody] User user)
 	    {
             if (ModelState.IsValid == false)
                 return BadRequest("Invalid data");
 
             try
             {
-                await _userManager.CreateAsync(user);
+                var _user = await _userManager.CreateAsync(user);
 
-                return Ok();
+                return Ok(_user);
             }
             catch (Exception ex)
             {
@@ -93,7 +112,7 @@ namespace DocsAPI.Controllers
                 if (updatedUser == null)
                     return NotFound();
 
-                return Ok();
+                return Ok(updatedUser);
             }
             catch (Exception ex)
             {
